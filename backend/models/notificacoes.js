d934c0d6611883af7e99f29e9f8154addbb3c0ba
Heloise <heloise.ssilva@gmail.com>
2025-08-06 11:28:36 -0300

    Alterações no bd e controllers de notificação

---
+++ backend/models/notificacoes.js
@@ -0,0 +1,48 @@
+import { create, readAll, update } from "../config/database";
+
+const criarNotificacao = async () => {
+    try {
+        return await create('notificacoes', {
+            usuario_id: usuario_id,
+            tecnico_id: tecnico_id,
+            mensagem,
+            status: 'nao_vista'
+        })
+        
+    } catch (error) {
+        console.error('Erro ao criar notificações: ', error);
+        throw error;
+        
+    }
+}
+
+const listarNotificacoesPorTecnico = async () => {
+    try {
+        return await readAll('notificacoes', `tecnico_id = ${tecnico_id} ORDER BY criado_em DESC`)
+        
+    } catch (error) {
+        console.error('Erro ao listar notificações: ', error);
+        throw error;
+    }
+}
+
+const listarNotificacoesPorUsuario = async () => {
+    try {
+        return await readAll('notificacoes', `usuario_id = ${usuario_id} ORDER BY criado_em DESC`)
+        
+    } catch (error) {
+        console.error('Erro ao listar notificaçõs por usuário', error);
+        throw error;
+    }
+};
+
+const marcarComoVista = async (id) => {
+    try {
+        return await update('notificacoes', { status: 'vista'}, `id = ${id}`)
+    } catch (error) {
+        console.error('Erro ao atualizar as notificações como vista: ', error);
+        throw error;
+    }
+}
+
+export {criarNotificacao, listarNotificacoesPorTecnico, listarNotificacoesPorUsuario, marcarComoVista};
