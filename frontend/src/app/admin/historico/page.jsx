a46e49dfced4f7b14a8b4f22615b2aaba8cadfc9
Isabella <isabellanunespaula@gmail.com>
2025-08-15 16:22:04 -0300

    Administrador

---
+++ frontend/src/app/admin/historico/page.jsx
@@ -0,0 +1,28 @@
+import styles from "@/app/admin/historico/historico.module.css";
+import HeaderAdmin from "@/components/HeaderAdmin/headerAdmin";
+
+export default function Informacoes() {
+    return (
+        <div className={styles.page}>
+    <HeaderAdmin />
+    
+    <div className="container-fluid p-4">
+        <div className={styles.tituloPrincipal}>
+            <h1>Histórico</h1>
+        </div>
+    <div className={styles.conteudoPrincipal}>
+        <div className={styles.chamados}>
+            <button>Novos Chamados</button>
+        </div>
+        <div className={styles.chamados}>
+            <button>Chamados Pendentes</button>
+        </div>
+        <div className={styles.chamados}>
+            <button>Chamados Concluídos</button>
+        </div>
+    </div>
+    </div>
+           
+</div>
+    );
+}
